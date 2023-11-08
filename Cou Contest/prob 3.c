4d35c3057edb52c83af987b79a10d512fd073b57
Ali <95523475+Ali007G@users.noreply.github.com>
2023-11-08 21:00:53 +0600

    Add files via upload

---
+++ Cou Contest/prob 3.c
@@ -0,0 +1,17 @@
+// Centurion
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d", &num1);
+
+    num2=100-num1;
+    printf("%d\n", num2);
+
+    return 0;
+}
