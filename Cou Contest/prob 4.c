7c6af78a4609ab12eea3d6e810ba9125e163a184
Ali <95523475+Ali007G@users.noreply.github.com>
2023-11-08 21:03:00 +0600

    Add files via upload

---
+++ Cou Contest/prob 4.c
@@ -0,0 +1,17 @@
+// Area Quest
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    int length, width, result;
+    scanf("%d %d",&length,&width);
+
+    result=length*width;
+    printf("%d", result);
+
+}
+
