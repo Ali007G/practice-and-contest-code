92ca4beed58438cb583c3752a3db3d747e23df05
Ali <95523475+Ali007G@users.noreply.github.com>
2023-11-08 21:00:33 +0600

    Add files via upload

---
+++ Cou Contest/prob 2.c
@@ -0,0 +1,17 @@
+// Star Plus
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    long int num1, num2, ans;
+    scanf("%ld %ld", &num1, &num2);
+
+    ans=num1+num2;
+    printf("%ld", ans);
+
+    return 0;
+}
