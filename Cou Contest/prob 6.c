6773c5e38345007d7bc92c6c8c30ac295d1c0394
Ali <95523475+Ali007G@users.noreply.github.com>
2023-11-09 23:05:57 +0600

    Add files via upload

---
+++ Cou Contest/prob 6.c
@@ -0,0 +1,20 @@
+// Unknown Village
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    float celsius,faren;
+    scanf("%f", &celsius);
+
+    faren=(9*celsius)/5+32;
+    printf("%.2f", faren);
+
+    return 0;
+}
+
+
+
