f3033c1dbf0a21fd592d05033ff00f0834e1c3b7
Ali <95523475+Ali007G@users.noreply.github.com>
2023-11-17 21:12:32 +0600

    Add files via upload

---
+++ Codeforces/Prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,n,count=0,a;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i=2;i<=n/2;i++)
+    {
+        scanf("%d", &a);
+        if(n%i==1)
+        count++;
+        {
+        printf("%d", n);
+    }
+}}
