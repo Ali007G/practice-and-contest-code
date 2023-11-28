b07fe0aa1f1f76f2d74d4dea8d19ba341ec5971d
Ali <95523475+Ali007G@users.noreply.github.com>
2023-11-28 22:33:32 +0600

    Add files via upload

---
+++ Many_More/1..c
@@ -0,0 +1,107 @@
+/*
+#include<stdio.h>
+int main()
+{
+    int a=7;
+    float b=5.5552;
+    char c = 'A';
+    double d=18.7483982;
+
+    printf("%d\n", a);
+    printf("%.2f\n", b);
+    printf("%c\n", c);
+    printf("%.4lf\n", d);
+
+    return 0;
+}
+*/
+/*
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    a=5;
+    printf("%d", a);
+}
+*/
+/*
+#include<stdio.h>
+int main()
+{
+    int x;
+    char y;
+    printf("Enter a number: ");
+    scanf("%d %c", &x, &y);
+    printf("The number is %d\n", x);
+    printf("The character is %c\n", y);
+
+ //  printf("The number is %d and The character is %c\n", x,y);
+    return 0;
+} */
+/*
+#include<stdio.h>
+int main()
+{
+    int x;
+    char y;
+    printf("Enter a number: ");
+    scanf("%d %c", &x, &y);
+    printf("The number is %d\n", x);
+    printf("The character is %c\n", y);
+ //  printf("The number is %d and The character is %c\n", x,y);
+    return 0;
+}
+*/
+/*
+#include<stdio.h>
+void main()
+{
+    int x,y,sum;
+    x=18,y=7;
+
+    sum=x+y;
+    printf("The sum is %d\n", sum);
+}
+*/
+/*
+#include<stdio.h>
+#define pi 3.1416
+void main()
+{
+    int r;
+    double area;
+    scanf("%d", &r);
+    area=pi*r*r;
+    printf("The circle area is %.2lf\n", area);
+}
+*/
+/*
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+   double number, squareRoot;
+
+   printf("Enter a number: ");
+   scanf("%lf", &number);
+
+   // computing the square root
+   squareRoot = sqrt(number);
+   printf("Square root of %.2lf =  %.2lf", number, squareRoot);
+
+   return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("       /\\         |            ---------   \n");
+    printf("      /  \\        |                |       \n");
+    printf("     /    \\       |                |       \n");
+    printf("    /------\\      |                |       \n");
+    printf("   /        \\     |                |       \n");
+    printf("  /          \\    |                |       \n");
+    printf(" /            \\   |__________  ____|_____  \n");
+
+}
